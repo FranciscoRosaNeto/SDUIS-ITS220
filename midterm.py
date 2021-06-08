fb88b255cbda1a84f10fdfb0d64c4049fbaf3b6d
FranciscoRosaNeto <84759201+FranciscoRosaNeto@users.noreply.github.com>
2021-06-07 17:45:54 -0700

    Add files via upload

---
+++ midterm.py
@@ -0,0 +1,6 @@
+print('Welcome to the Math game!')
+
+num1 = int(input('Enter first number you want to sum:'))
+num2 = int(input('Enter second number:'))
+result = num1 + num2
+print("{0} + {1} = {2}". format(num1,num2,result))
